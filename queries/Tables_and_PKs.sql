d7cd58e8eb95d4f3e142e3103337580e7777407d
malaths6 <stelios.malathouras@gmail.com>
2025-03-14 16:19:23 +0200

    Update Tables_and_PKs.sql

--- queries/Tables_and_PKs.sql
+++ queries/Tables_and_PKs.sql
@@ -1,10 +1,10 @@
-SELECT n.nspname,
-       t.relname as table, 
-       c.conname as pk_name
-  FROM pg_class t
-  JOIN pg_constraint c ON ( c.conrelid = t.OID AND c.contype = 'p')
-  JOIN pg_namespace n  ON (n.oid = t.relnamespace)
- WHERE relkind = 'r'
-   AND t.relname NOT LIKE 'pg_%'
-   AND t.relname NOT LIKE 'sql_%'
-   ORDER BY n.nspname, t.relname, c.conname;
+SELECT n.nspname AS schema_name,
+       t.relname AS table_name, 
+       c.conname AS pk_name
+FROM pg_class t
+JOIN pg_constraint c ON (c.conrelid = t.oid AND c.contype = 'p')
+JOIN pg_namespace n ON (n.oid = t.relnamespace)
+WHERE t.relkind = 'r'
+  AND t.relname NOT LIKE 'pg\_%'
+  AND t.relname NOT LIKE 'sql\_%'
+ORDER BY n.nspname, t.relname, c.conname;
